0bc17a7a000e52dfa604fb891a1118141cac962a
Strange <sithugolang@gmail.com>
2025-11-30 12:16:24 +0700

    feat: Implement initial gRPC server setup with graceful shutdown and add required dependencies.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,48 @@
 package main
 
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/sithu-go/ride-share/contracts/env"
+	"google.golang.org/grpc"
+)
+
+var (
+	grpcAddr = env.GetString("GRPC_ADDR", "0.0.0.0:9093")
+)
+
 func main() {
-	// Entry point
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+		<-sigCh
+		cancel()
+	}()
+
+	lis, err := net.Listen("tcp", grpcAddr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	go func() {
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+	log.Println("Shutting down trip service")
+	grpcServer.GracefulStop()
+
 }
